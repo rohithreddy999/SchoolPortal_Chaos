315262f843973e38169ff84d4f711d57cf413817
byreddy1303 <byreddy1303@gmail.com>
2026-04-21 22:36:57 +0530

    clearbranch

--- app/core/constants.py
+++
@@ -1,4 +0,0 @@
-FEE_HEADS = ("admission", "term1", "term2", "term3", "transport", "books")
-ONLINE_PAYMENT_MODES = ("upi", "card", "netbanking", "wallet")
-OFFLINE_PAYMENT_MODES = ("cash", "cheque", "bank_transfer", "offline")
-MAX_MONEY = "99999999.99"
